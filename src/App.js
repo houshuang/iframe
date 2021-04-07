b54df1f5ed58999158d3711d14f4bc0000a312d3
Stian Haklev <stian@minervaproject.com>
2021-04-07 16:15:24 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import WordCloud from "./Wordcloud";
 import Query from "./Query";
 import Chat from "./Chat";
